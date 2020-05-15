cd43b567b63cd06806ee5e67d4a2953b7aad4acd
Clayton Robbins <robbinsclay@gmail.com>
2020-05-14 22:54:54 -0400

    Added the while statement.

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -3,9 +3,9 @@ print("Welcome to the guessing game!")
 print("You will have to guess a number between 0 and 10")
 number = random.randint(0,10)
 guess = input("What is your guess?")
-if guess > number:
-    print("The guess was too high")
-elif guess < number:
-    print("The guess was too low")
-else:
-    print("Congradulations you guessed the number!")
+while number != guess
+    if guess > number:
+        print("The guess was too high")
+    elif guess < number:
+        print("The guess was too low")
+print("Congradulations you guessed the number!")
