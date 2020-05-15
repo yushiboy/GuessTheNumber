e5525129b66a223462944c1b35f5264fc8829f6c
Clayton Robbins <robbinsclay@gmail.com>
2020-05-14 21:18:23 -0400

    First try.

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,2 +1,11 @@
+import random
 print("Welcome to the guessing game!")
 print("You will have to guess a number between 0 and 10")
+number = random.randint(0,10)
+guess = input("What is your guess?")
+if guess > number:
+    print("The guess was too high")
+elif guess < number:
+    print("The guess was too low")
+else:
+    print("Congradulations you guessed the number!")
