5d03d1b5710fb415cee6d634de17d1ad688d02bf
Clayton Robbins <robbinsclay@gmail.com>
2020-05-14 23:04:13 -0400

    Debug the while fn.

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,11 +1,17 @@
 import random
 print("Welcome to the guessing game!")
 print("You will have to guess a number between 0 and 10")
+guess = 11
 number = random.randint(0,10)
-guess = input("What is your guess?")
-while number != guess
-    if guess > number:
-        print("The guess was too high")
-    elif guess < number:
-        print("The guess was too low")
-print("Congradulations you guessed the number!")
+userguess = input("What is your guess?")
+guess = int(userguess)
+while guess != number:
+    if guess < number:
+        print("Guess too low")
+        userguess = input("What is your guess?")
+        guess = int(userguess)
+    elif guess > number:
+        print("Guess too high")
+        userguess = input("What is your guess?")
+        guess = int(userguess)
+print("You successfully guessed the number")
